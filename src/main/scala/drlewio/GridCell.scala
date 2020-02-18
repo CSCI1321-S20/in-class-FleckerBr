bd3a26d63d36147c73e218aa277702425e17682e
bflecker <bflecker@trinity.edu>
2020-02-18 10:57:43 -0600

    Initial update

---
+++ src/main/scala/drlewio/GridCell.scala
@@ -0,0 +1,9 @@
+package drlewio
+
+import scalafx.scene.canvas.GraphicsContext
+
+trait GridCell{
+    def x: Int
+    def y: Int
+    def color: ColorOption.Value
+}
--- src/main/scala/drlewis/GridCell.scala
+++
@@ -1,3 +0,0 @@
-package drlewis
-
-class GridCell(val x: Int, val y: Int, val color: ColorOption)
